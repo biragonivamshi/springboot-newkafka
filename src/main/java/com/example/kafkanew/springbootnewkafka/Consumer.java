c673e90d5219a1dc89d913bf200b53993b2b5cb6
biragonivamshi <vamshigoud206@gmail.com>
2023-07-26 15:03:21 +0530

    removed log in main method

--- src/main/java/com/example/kafkanew/springbootnewkafka/Consumer.java
+++ src/main/java/com/example/kafkanew/springbootnewkafka/Consumer.java
@@ -1,12 +1,26 @@
 package com.example.kafkanew.springbootnewkafka;
 
+//import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+
+
 @Service
 public class Consumer {
+    
+    	
     @KafkaListener(topics = "test_topic", groupId = "group_id")
+    @CircuitBreaker(name="consumeMessage", fallbackMethod = "handleFallback")
     public void consumeMessage(String message){
-        System.out.println(message);
+    	if(message.equals("error"))
+            throw new RuntimeException("Kafka service is down.");
+    	else
+    		System.out.println(message);
+    }
+    
+    public void handleFallback(String message,Exception e) {
+         System.out.println("Fallback method triggered: Error occurred during message processing.");
     }
 }
--- src/main/java/com/example/kafkanew/springbootnewkafka/SpringbootNewkafkaApplication.java
+++ src/main/java/com/example/kafkanew/springbootnewkafka/SpringbootNewkafkaApplication.java
@@ -2,12 +2,14 @@ package com.example.kafkanew.springbootnewkafka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+
 public class SpringbootNewkafkaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootNewkafkaApplication.class, args);
+		System.out.println("kafka running..");
 	}
-
 }
